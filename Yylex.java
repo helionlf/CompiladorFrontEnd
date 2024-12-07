bb1d3c4608d53e88ab9023d7f7f47833990c09c2
Hélio Nogueira <heliofilhofhnlf@gmail.com>
2024-12-07 10:18:11 -0300

    add yytext

--- Yylex.java
+++ Yylex.java
@@ -681,132 +681,132 @@ class Yylex {
           // fall through
           case 32: break;
           case 4:
-            { System.out.println("LPAREN");
+            { System.out.println("LPAREN - Lexema: " + yytext());
             }
           // fall through
           case 33: break;
           case 5:
-            { System.out.println("RPAREN");
+            { System.out.println("RPAREN - Lexema: " + yytext());
             }
           // fall through
           case 34: break;
           case 6:
-            { System.out.println("TIMES");
+            { System.out.println("TIMES - Lexema: " + yytext());
             }
           // fall through
           case 35: break;
           case 7:
-            { System.out.println("PLUS");
+            { System.out.println("PLUS - Lexema: " + yytext());
             }
           // fall through
           case 36: break;
           case 8:
-            { System.out.println("COMMA");
+            { System.out.println("COMMA - Lexema: " + yytext());
             }
           // fall through
           case 37: break;
           case 9:
-            { System.out.println("MINUS");
+            { System.out.println("MINUS - Lexema: " + yytext());
             }
           // fall through
           case 38: break;
           case 10:
-            { System.out.println("DIVIDE");
+            { System.out.println("DIVIDE - Lexema: " + yytext());
             }
           // fall through
           case 39: break;
           case 11:
-            { System.out.println("Digito");
+            { System.out.println("Digito - Lexema: " + yytext());
             }
           // fall through
           case 40: break;
           case 12:
-            { System.out.println("SEMI");
+            { System.out.println("SEMI - Lexema: " + yytext());
             }
           // fall through
           case 41: break;
           case 13:
-            { System.out.println("LT");
+            { System.out.println("LT - Lexema: " + yytext());
             }
           // fall through
           case 42: break;
           case 14:
-            { System.out.println("ATRIBUICAO");
+            { System.out.println("ATRIBUICAO - Lexema: " + yytext());
             }
           // fall through
           case 43: break;
           case 15:
-            { System.out.println("GT");
+            { System.out.println("GT - Lexema: " + yytext());
             }
           // fall through
           case 44: break;
           case 16:
-            { System.out.println("Identificador");
+            { System.out.println("Identificador - Lexema: " + yytext());
             }
           // fall through
           case 45: break;
           case 17:
-            { System.out.println("LCURLY");
+            { System.out.println("LCURLY - Lexema: " + yytext());
             }
           // fall through
           case 46: break;
           case 18:
-            { System.out.println("RCURLY");
+            { System.out.println("RCURLY - Lexema: " + yytext());
             }
           // fall through
           case 47: break;
           case 19:
-            { System.out.println("NEQ");
+            { System.out.println("NEQ - Lexema: " + yytext());
             }
           // fall through
           case 48: break;
           case 20:
-            { System.out.println("Float");
+            { System.out.println("Float - Lexema: " + yytext());
             }
           // fall through
           case 49: break;
           case 21:
-            { System.out.println("LE");
+            { System.out.println("LE - Lexema: " + yytext());
             }
           // fall through
           case 50: break;
           case 22:
-            { System.out.println("EQ");
+            { System.out.println("EQ - Lexema: " + yytext());
             }
           // fall through
           case 51: break;
           case 23:
-            { System.out.println("GE");
+            { System.out.println("GE - Lexema: " + yytext());
             }
           // fall through
           case 52: break;
           case 24:
-            { System.out.println("IF");
+            { System.out.println("IF - Lexema: " + yytext());
             }
           // fall through
           case 53: break;
           case 25:
-            { System.out.println("INT");
+            { System.out.println("INT - Lexema: " + yytext());
             }
           // fall through
           case 54: break;
           case 26:
-            { System.out.println("ELSE");
+            { System.out.println("ELSE - Lexema: " + yytext());
             }
           // fall through
           case 55: break;
           case 27:
-            { System.out.println("FLOAT");
+            { System.out.println("FLOAT - Lexema: " + yytext());
             }
           // fall through
           case 56: break;
           case 28:
-            { System.out.println("WHILE");
+            { System.out.println("WHILE - Lexema: " + yytext());
             }
           // fall through
           case 57: break;
           case 29:
-            { System.out.println("RETURN");
+            { System.out.println("RETURN - Lexema: " + yytext());
             }
           // fall through
           case 58: break;
--- calcPrint.flex
+++ calcPrint.flex
@@ -16,32 +16,32 @@ COMMENT_MULTI = "/*"([^*]|[*][^/])*"*/"
 
 <YYINITIAL> {
     {WHITESPACE}                  { /**/ }
-    "if"                          { System.out.println("IF"); }
-    "else"                        { System.out.println("ELSE"); }
-    "while"                       { System.out.println("WHILE"); }
-    "return"                      { System.out.println("RETURN"); }
-    "int"                         { System.out.println("INT"); }
-    "float"                       { System.out.println("FLOAT"); }
-    {ID}                          { System.out.println("Identificador"); }
-    {DIGIT}                       { System.out.println("Digito"); }
-    {FLOAT}                       { System.out.println("Float"); }
-    "=="                          { System.out.println("EQ"); }
-    "="                          { System.out.println("ATRIBUICAO"); }
-    "!="                          { System.out.println("NEQ"); }
-    "<"                           { System.out.println("LT"); }
-    "<="                          { System.out.println("LE"); }
-    ">"                           { System.out.println("GT"); }
-    ">="                          { System.out.println("GE"); }
-    "+"                           { System.out.println("PLUS"); }
-    "-"                           { System.out.println("MINUS"); }
-    "*"                           { System.out.println("TIMES"); }
-    "/"                           { System.out.println("DIVIDE"); }
-    ";"                           { System.out.println("SEMI"); }
-    ","                           { System.out.println("COMMA"); }
-    "{"                           { System.out.println("LCURLY"); }
-    "}"                           { System.out.println("RCURLY"); }
-    "("                           { System.out.println("LPAREN"); }
-    ")"                           { System.out.println("RPAREN"); }
+    "if"                          { System.out.println("IF - Lexema: " + yytext()); }
+    "else"                        { System.out.println("ELSE - Lexema: " + yytext()); }
+    "while"                       { System.out.println("WHILE - Lexema: " + yytext()); }
+    "return"                      { System.out.println("RETURN - Lexema: " + yytext()); }
+    "int"                         { System.out.println("INT - Lexema: " + yytext()); }
+    "float"                       { System.out.println("FLOAT - Lexema: " + yytext()); }
+    {ID}                          { System.out.println("Identificador - Lexema: " + yytext()); }
+    {DIGIT}                       { System.out.println("Digito - Lexema: " + yytext()); }
+    {FLOAT}                       { System.out.println("Float - Lexema: " + yytext()); }
+    "=="                          { System.out.println("EQ - Lexema: " + yytext()); }
+    "="                          { System.out.println("ATRIBUICAO - Lexema: " + yytext()); }
+    "!="                          { System.out.println("NEQ - Lexema: " + yytext()); }
+    "<"                           { System.out.println("LT - Lexema: " + yytext()); }
+    "<="                          { System.out.println("LE - Lexema: " + yytext()); }
+    ">"                           { System.out.println("GT - Lexema: " + yytext()); }
+    ">="                          { System.out.println("GE - Lexema: " + yytext()); }
+    "+"                           { System.out.println("PLUS - Lexema: " + yytext()); }
+    "-"                           { System.out.println("MINUS - Lexema: " + yytext()); }
+    "*"                           { System.out.println("TIMES - Lexema: " + yytext()); }
+    "/"                           { System.out.println("DIVIDE - Lexema: " + yytext()); }
+    ";"                           { System.out.println("SEMI - Lexema: " + yytext()); }
+    ","                           { System.out.println("COMMA - Lexema: " + yytext()); }
+    "{"                           { System.out.println("LCURLY - Lexema: " + yytext()); }
+    "}"                           { System.out.println("RCURLY - Lexema: " + yytext()); }
+    "("                           { System.out.println("LPAREN - Lexema: " + yytext()); }
+    ")"                           { System.out.println("RPAREN - Lexema: " + yytext()); }
     {COMMENT_SINGLE}              { /**/ }
     {COMMENT_MULTI}               { /**/ }
     .                             { System.err.println("Illegal character: " + yytext()); }
